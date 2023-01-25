77c95e1087f778dda9713c729db11b8979d4dd70
kkangs <1003gorkd@naver.com>
2023-01-25 18:17:04 +0900

    refactor : animation 적용

--- src/pages/sections/Experience.tsx
+++ src/pages/sections/Experience.tsx
@@ -1,17 +1,25 @@
-import Card from '@/components/Card';
-import EXPERIENCE_LIST from '@/constants/experience';
+import { useRef } from 'react';
 import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
 
+import EXPERIENCE_LIST from '@/constants/experience';
+
+import Animation from '@/components/Animation';
+import Card from '@/components/Card';
+
 const Experience = () => {
+  const animationRef = useRef(null);
+
   return (
-    <div>
+    <div ref={animationRef}>
       <h2 className="title">경험</h2>
 
       <ResponsiveMasonry columnsCountBreakPoints={{ 0: 1, 600: 2, 1200: 3 }}>
         <Masonry gutter="24px">
-          {EXPERIENCE_LIST.map((experience) => {
-            return <Card key={experience.id} {...experience} />;
-          })}
+          {EXPERIENCE_LIST.map((experience, idx) => (
+            <Animation key={experience.id} direction="bottom" delay={idx * 300} target={animationRef}>
+              <Card key={experience.id} {...experience} />
+            </Animation>
+          ))}
         </Masonry>
       </ResponsiveMasonry>
     </div>
--- src/pages/sections/Project.tsx
+++ src/pages/sections/Project.tsx
@@ -1,17 +1,25 @@
-import Card from '@/components/Card';
-import PROJECT_LIST from '@/constants/project';
+import { useRef } from 'react';
 import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
 
+import PROJECT_LIST from '@/constants/project';
+
+import Animation from '@/components/Animation';
+import Card from '@/components/Card';
+
 const Project = () => {
+  const animationRef = useRef(null);
+
   return (
-    <div>
+    <div ref={animationRef}>
       <h2 className="title">프로젝트</h2>
 
       <ResponsiveMasonry columnsCountBreakPoints={{ 0: 1, 600: 2, 1200: 3 }}>
         <Masonry gutter="24px">
-          {PROJECT_LIST.map((project) => {
-            return <Card key={project.id} {...project} />;
-          })}
+          {PROJECT_LIST.map((project, idx) => (
+            <Animation key={project.id} direction="bottom" delay={idx * 300} target={animationRef}>
+              <Card key={project.id} {...project} />
+            </Animation>
+          ))}
         </Masonry>
       </ResponsiveMasonry>
     </div>
