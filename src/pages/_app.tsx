4e299cf9a5f7304220d9ced00ee79c85c429439c
kkangs <1003gorkd@naver.com>
2023-01-25 18:22:23 +0900

    refactor : favicon 변경 및 meta 태그 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,22 @@ import '@/styles/index.scss';
 import { SectionContextProvider } from '@/contexts/SectionContext';
 import Header from '@/shared/header/Header';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SectionContextProvider>
-      <Header />
-      <Component {...pageProps} />
-    </SectionContextProvider>
+    <>
+      <Head>
+        <title>전해강 | 포트폴리오</title>
+        <meta name="description" content="주니어 프론트엔드 개발자 전해강 입니다." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <SectionContextProvider>
+        <Header />
+        <Component {...pageProps} />
+      </SectionContextProvider>
+    </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Section from '@/components/Section';
-import Head from 'next/head';
 import Experience from './sections/Experience';
 import Intro from './sections/Intro';
 import Project from './sections/Project';
@@ -7,13 +6,6 @@ import Project from './sections/Project';
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>전해강 | 포트폴리오</title>
-        <meta name="description" content="주니어 프론트엔드 개발자 전해강 입니다." />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <Section name="Intro" position="center" full>
         <Intro />
       </Section>
