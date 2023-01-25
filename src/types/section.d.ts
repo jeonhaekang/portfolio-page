1fda0089d600612a354b2c7cdbb5b648dbb93a76
kkangs <1003gorkd@naver.com>
2023-01-25 13:17:51 +0900

    feat : section 관련 type 파일 생성

---
+++ src/types/section.d.ts
@@ -0,0 +1,14 @@
+export interface ISection {
+  id: number;
+  name: string;
+  element: HTMLElement;
+}
+
+export type TRegister = (name: string) => {
+  ref: (element: HTMLElement) => void;
+};
+
+export interface IActions {
+  sections: MutableRefObject<ISection[]>;
+  register: TRegister;
+}
