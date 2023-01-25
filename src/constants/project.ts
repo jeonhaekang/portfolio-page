a8da2bb3e4e84a97b4182d74b37b85ddff81a559
kkangs <1003gorkd@naver.com>
2023-01-25 19:18:52 +0900

    refactor : project 추가

--- src/constants/project.ts
+++ src/constants/project.ts
@@ -4,14 +4,22 @@ import { ICard } from '@/types/common';
 const PROJECT_LIST: ICard[] = [
   {
     id: 1,
-    imageUrl: getImageUrl('portfolio.png'),
-    title: '포트폴리오',
-    subTitle: '포트폴리오 사이트',
-    desc: '현재 보시고 계시는 사이트입니다.\n\n 인터렉션 등 애니메이션 기능을 라이브러리 없이 구현해 보는 연습을 진행하였습니다.\n\n 반응형 웹 사이트로 구성되어 있습니다. \n\n React, Javascript, Scss를 사용하였습니다.',
-    link: 'https://github.com/jeonhaekang/portfolio-page',
+    imageUrl: getImageUrl('chatting.png'),
+    title: '채팅 어플리케이션',
+    subTitle: '데스크탑 채팅 어플리케이션',
+    desc: '데스크탑에 설치하여 사용하는 채팅 어플리케이션 입니다. \n\n React를 사용해 데스크탑 어플리케이션을 구현해 보는 경험을 하였습니다. \n\n React, Typescript, Scss, Nextron을 사용하였습니다.',
+    link: 'https://github.com/jeonhaekang/desktop_chat_app',
   },
   {
     id: 2,
+    imageUrl: getImageUrl('portfolio.png'),
+    title: '포트폴리오',
+    subTitle: '포트폴리오 사이트',
+    desc: '현재 보시고 계시는 사이트입니다.\n\n 인터렉션 등 애니메이션 기능을 구현해 보는 연습을 진행하였으며.\n 반응형 웹 사이트로 구성되어 있습니다. \n\n Typescript, Next.js를 경험하였습니다. \n\n 현재는 원페이지 이지만 추후 확장성을 고려해 SEO에 유리한 Next.js를 채택하였습니다.  \n\n React, Typescript, Scss, Next.js를 사용하였습니다.',
+    link: 'https://github.com/jeonhaekang/portfolio-page',
+  },
+  {
+    id: 3,
     imageUrl: getImageUrl('careerwallet.png'),
     title: '커리어월렛',
     subTitle: '취업 플랫폼',
@@ -19,7 +27,7 @@ const PROJECT_LIST: ICard[] = [
     link: 'https://careerwallet.co.kr/',
   },
   {
-    id: 3,
+    id: 4,
     imageUrl: getImageUrl('dalking.png'),
     title: '덕킹',
     subTitle: '실시간 채팅 플랫폼',
