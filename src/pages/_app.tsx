f048690c0015c206ffb41077ba89ba67843f69a9
kkangs <1003gorkd@naver.com>
2023-01-25 15:05:39 +0900

    feat : header 연결

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
-import { SectionContextProvider } from '@/contexts/SectionContext';
 import '@/styles/index.scss';
+import { SectionContextProvider } from '@/contexts/SectionContext';
+import Header from '@/shared/header/Header';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SectionContextProvider>
+      <Header />
       <Component {...pageProps} />
     </SectionContextProvider>
   );
